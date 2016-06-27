89cf5599fe5b7e5fd20173dd632aebb1dea19bd0
Thomas Wade <w1220w@126.com>
2016-06-27 18:18:28 +0800

    20160627

--- gulpfile.js
+++ gulpfile.js
@@ -26,7 +26,7 @@ gulp.task('default',function(callback){
 gulp.task('browserSync',function(){
 	browserSync.init({
         server: {
-            baseDir: "./wheel/wheel_02_20160624"
+            baseDir: "./wheel"
         }
     });
 /*	browserSync({
