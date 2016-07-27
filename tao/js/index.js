de2dc809c702652963fafd91b2ba96b8119e87a6
Thomas Wade <w1220w@126.com>
2016-07-27 17:14:33 +0800

    在线记账

--- tao/js/index.js
+++ tao/js/index.js
@@ -3,88 +3,77 @@ $(document).ready(function(){
 	newsImg();
 	
 	
-	scrollLink(20,1500);
+	
 	dropDown();
 	floatWindowClose();
 	switchProduct();
 	quickBar();
 });
-function scrollLink(speed,delay) {   //函数 函数名(变量1,变量2,变量3)滚动高度,速度,延迟
-		var p=false; //定义变量 p为假;
-		var t; //定义变量t了
-		var $ul=$("#hotPicRow ul"); //定义变量o为document.getElementByIdx_x("marqueebox")对象
-		var ul = $ul[0];
-		var blockNum = $(".picRow02 ul li").length;
-		if(blockNum<4){return}
+function scrollLink(speed,delay,ele) {   //函数 函数名(变量1,变量2,变量3)滚动高度,速度,延迟
+            var p=false; //定义变量 p为假;
+            var t; //定义变量t了
+            var $ul=$(ele+" ul");  //定义变量o为document.getElementByIdx_x("marqueebox")对象
+            var ul = $ul[0];
+            var blockNum = $ul.children("li").length;
+            //if(blockNum<4){return}
 
-		var lengthUl = ul.scrollWidth;
-		var singleBlock = lengthUl/(blockNum);
-		
-		var index;
-		
-		var array = new Array();
-		for(var i= 1;i<=blockNum;i++){
-			array.push(i*singleBlock)
-		};
-		//if(!o)return;
-		var list = $ul.html();
-		
-		$ul.append(list).prepend(list); //设置变量o的innerHTML对象为自加;设置为类似123123的队列。当高度大于
-		//一般，即经过一半的队列（个数）时，马上设置为margintop=0的初始位置，因为位于第二段的开始，所以没有抖动。
-		
-		ul.style.marginLeft=-lengthUl+'px'; //o的属性，设置或获取对象的上边距宽度
-		
-		ul.onmouseover=function(){p=true;} 
-		ul.onmouseout=function(){p=false;} 
+            var lengthUl = ul.scrollWidth;
+            var singleBlock = lengthUl/(blockNum);
+             var arrowLeft = $(ele).find(".arrow_left");
+            var arrowRight = $(ele).find(".arrow_right");
+            
+            var index;
+            
+            var array = new Array();
+            for(var i= 1;i<=blockNum;i++){
+              array.push(i*singleBlock)
+            };
+            
+            var list = $ul.html();
+            
+            $ul.append(list).prepend(list); //
+            
+            ul.style.marginLeft=-lengthUl+'px'; //o的属性，设置或获取对象的上边距宽度
+            
+            ul.onmouseover=function(){p=true;} 
+            ul.onmouseout=function(){p=false;} 
 
-		function start(){ //“开始”函数
-	//		t=setInterval(scrolling,speed); //给前面定义的变量t赋值=自动（滚动，速度）
-			//speed：在间隔短时间（如20ms）移动1像素，模拟速度
-	//		if(!p) ul.style.marginLeft=parseInt(ul.style.marginLeft)-1+"px"; //当P为空，属性o的上边距宽度为数字[把变量o上边距宽度转为integer类型，其中parseInt功能为转换]
-			function arrow(){
-				$(".arrow_right").on("click",function(){
-								getIndex()
-								
-								/*if(index==blockNum){index=1;ul.style.marginLeft=-lengthUl+'px';}
-									else{index+=1}
-							*/
-							if(!$ul.is(":animated"))
-							$ul.stop(false,true).animate({'margin-left':'-='+singleBlock},{duration:'fast',complete:function(){if(index==blockNum){index=1;ul.style.marginLeft=-lengthUl+'px';}}})
-				});
-				$(".arrow_left").on("click",function(){
-								getIndex();
-							
-							if(!$ul.is(":animated"))										
-							$ul.stop(false,true).animate({'margin-left':'+='+singleBlock},{duration:'fast',complete:function(){	if(index==blockNum){index=1;ul.style.marginLeft=-lengthUl+'px';}}})
-				})
-			}
-			arrow();
-		}	
-		function getIndex(){
-				var marginLeft = ul.style.marginLeft;
-				for(var i = 1;i<=blockNum;i++){
-					
-				     	if(Math.abs(parseInt(marginLeft)+lengthUl)<array[i-1]){
-				     		index = i;
-				     		
-				     		return ;
-				     	}
-				     	
-				};
-				
-		}
-		function scrolling(){ //“滚动”函数
-			var marginLeft = ul.style.marginLeft;
-		//	if(!p)ul.style.marginLeft=parseInt(marginLeft)-1+"px"; //上边距宽度为数字[把变量o上边距宽度转为integer类型，其中parseInt功能为转换]
-		//	if(Math.abs(parseInt(marginLeft))>=(ul.scrollWidth/3*2)) ul.style.marginLeft=0; //如果计算出来的上边距宽度大于或者等于滚动的高度除以2 则o的上边距宽度为0
-			
-			
-			
-			
-			}
+            function start(){ 
+              function arrow(){
+                arrowRight.on("click",function(){
+                        getIndex()
+                       
+                      if(!$ul.is(":animated"))
+                      $ul.stop(false,true).animate({'margin-left':'-='+singleBlock},{duration:'fast',complete:function(){if(index==blockNum){index=1;ul.style.marginLeft=-lengthUl+'px';}}})
+                });
+                arrowLeft.on("click",function(){
+                        getIndex();
+                      
+                      if(!$ul.is(":animated"))                    
+                      $ul.stop(false,true).animate({'margin-left':'+='+singleBlock},{duration:'fast',complete:function(){ if(index==blockNum){index=1;ul.style.marginLeft=-lengthUl+'px';}}})
+                })
+              }
+              arrow();
+            } 
+            function getIndex(){
+                var marginLeft = ul.style.marginLeft;
+                for(var i = 1;i<=blockNum;i++){
+                  
+                      if(Math.abs(parseInt(marginLeft)+lengthUl)<array[i-1]){
+                        index = i;
+                        
+                        return ;
+                      }
+                      
+                };
+                
+            }
+            function scrolling(){ //“滚动”函数
+              var marginLeft = ul.style.marginLeft;
+            }
 
-		start();
-};
+            start();
+        };
 function quickBar(){
 	/*电话*/
 
