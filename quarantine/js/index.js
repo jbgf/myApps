5daa918dabcb67ae73cb82a3279966a77ffbcd35
Thomas Wade <w1220w@126.com>
2016-09-26 17:42:46 +0800

    sync

--- quarantine/js/index.js
+++ quarantine/js/index.js
@@ -3,5 +3,29 @@ $(function(){
           jQuery(".slideBox").slide({mainCell:".bd ul",autoPlay:true,effect:"leftLoop",easing:"swing",interTime:"3500"});
           //最新公告文字滚动
            jQuery(".txtScroll-left").slide({titCell:".hd ul",mainCell:".bd ul",autoPage:true,effect:"leftLoop",autoPlay:true,scroll:2,vis:2,trigger:"click"});
-            
+          //news滚动
+          jQuery("#newsBox01").slide({mainCell:".bd ul",autoPlay:true,effect:"leftLoop",easing:"swing",interTime:"3500"});  
+          //news tab切换
+          jQuery(".slideTxtBox").slide();
+          
+
+          //服务支持
+          $('.content01 .floor:not(.on)').hover(function(){
+              $(this).addClass("on")
+           },function(){
+              $(this).removeClass("on")
+           })  
+
+          //产品检测范围
+           $('.rp-img:not(.on)').hover(function(){
+              $(this).addClass("on")
+           },function(){
+              $(this).removeClass("on")
+           })
+           //关于我们
+           $('.abtn-wrapper:not(.on)').hover(function(){
+              $(this).addClass("on")
+           },function(){
+              $(this).removeClass("on")
+           })
 })
