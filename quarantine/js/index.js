ef71c382d3a26180c0fbfd793e358ed258383c7d
Thomas Wade <w1220w@126.com>
2016-10-08 15:39:27 +0800

    sync

--- quarantine/js/index.js
+++ quarantine/js/index.js
@@ -1,4 +1,16 @@
 $(function(){
+          //欢迎栏
+          showTime();
+          //导航栏
+          
+
+          var st = 180;
+          $('.navbar-block').mouseenter(function () {
+              $(this).find('ul').stop(false, true).slideDown(st);
+          }).mouseleave(function () {
+              $(this).find('ul').stop(false, true).slideUp(st);
+          });
+
           //banner 轮播
           jQuery(".slideBox").slide({mainCell:".bd ul",autoPlay:true,effect:"leftLoop",easing:"swing",interTime:"3500"});
           //最新公告文字滚动
@@ -9,6 +21,7 @@ $(function(){
           jQuery(".slideTxtBox").slide();
           
 
+/*r-sider01*/
           //服务支持
           $('.content01 .floor:not(.on)').hover(function(){
               $(this).addClass("on")
@@ -22,10 +35,76 @@ $(function(){
            },function(){
               $(this).removeClass("on")
            })
-           //关于我们
+           //关于我们,右
            $('.abtn-wrapper:not(.on)').hover(function(){
               $(this).addClass("on")
            },function(){
               $(this).removeClass("on")
            })
+
+/*左侧边栏 */
+    /*l-sider01*/
+            $(".l-sider01 .floor:not(.f00,.on)").hover(function(){
+              $(this).addClass("on")
+           },function(){
+              $(this).removeClass("on")
+           })
+    /*l-sider02*/
+            $(".l-sider02 .floor").on("click",function(){
+                $(this).toggleClass("current").siblings('.list').slideToggle();
+            })
+
+            $(".l-sider02 .list .item:not(.on)").hover(function(){
+              $(this).addClass("on")
+           },function(){
+              $(this).removeClass("on")
+           })
+
+/*左侧边栏 end     */
+            $(".pagination ul li:not(.right,.left,.on,.ellipsis)").hover(function(){
+              $(this).addClass("on")
+           },function(){
+              $(this).removeClass("on")
+           })
+
+           /*产品检测方案及背景介绍切换*/ 
+           $(".hbtnBox .hbtn").hover(function(){
+              var on = $(this).hasClass('on');
+              if(on){return}else{
+                $(this).addClass('on').siblings('.hbtn').removeClass('on');
+
+                var textBody = $(this).parents(".r-row").find(".text-body");
+                textBody.toggle();
+              }
+
+              
+           })
+           /*测试范围详情*/
+           $(".riRow").click(function(){
+              $(this).find(".riRow-t02").slideToggle("fast");
+           })
+
+           function showTime(){ 
+            var show_day=new Array('星期一','星期二','星期三','星期四','星期五','星期六','星期日'); 
+            var time=new Date(); 
+            var year=time.getFullYear(); 
+            var month=time.getMonth(); 
+            var date=time.getDate(); 
+            var day=time.getDay(); 
+            var hour=time.getHours(); 
+            var minutes=time.getMinutes(); 
+            var second=time.getSeconds(); 
+            month=month+1; 
+            month<10?month='0'+month:month; 
+            
+            hour<10?hour='0'+hour:hour; 
+            minutes<10?minutes='0'+minutes:minutes; 
+            second<10?second='0'+second:second; 
+            var now_time=year+'年'+month+'月'+date+'日'+' '+show_day[day-1]+' '+hour+':'+minutes+':'+second; 
+            var st = document.getElementById('showtime');
+            if(st)st.innerHTML=now_time; 
+            setTimeout(showTime,1000); 
+        
+          };
+
 })
