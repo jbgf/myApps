9fb0f6be88e483d6ae2c81e9c360413161bf9a3c
Thomas Wade <w1220w@126.com>
2016-10-14 18:07:24 +0800

    sync

---
+++ quarantine/m/js/index.js
@@ -0,0 +1,26 @@
+$(function(){
+	//banner swiper轮播
+	var mySwiper = new Swiper ('#swiper-container01', {
+	    autoplay:5000,
+	    loop: true,
+	    pagination: '#swiper-pagination01'
+
+	}) 
+
+
+	//news tab切换
+	var mySwiper02 = new Swiper ('#swiper-container02', {
+	    /*autoplay:5000,*/
+	    loop: true,
+	    slidesPerView:3,
+	    slideToClickedSlide:true,
+	})
+	var mySwiper03 = new Swiper ('#swiper-container03', {
+	    loop: true,
+	    slidesPerView:1,
+	    loopAdditionalSlides:2   //复制后的slide数量要一样
+	})
+	mySwiper02.params.control = mySwiper03;  /*双向控制*/
+	mySwiper03.params.control = mySwiper02;
+	
+})
