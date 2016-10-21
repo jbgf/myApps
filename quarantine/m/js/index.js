e104a00cf40d53b65c742b888305c8a7a65b1b1d
Thomas Wade <w1220w@126.com>
2016-10-21 17:23:18 +0800

    sync

--- quarantine/m/js/index.js
+++ quarantine/m/js/index.js
@@ -1,6 +1,4 @@
 $(function(){
-	
-
 	//banner swiper轮播
 	var mySwiper = new Swiper ('#swiper-container01', {
 	    autoplay:5000,
@@ -9,24 +7,18 @@ $(function(){
 
 	}) 
 
-	//news tab切换
-	var mySwiper02 = new Swiper ('#swiper-container02', {
-	    /*autoplay:5000,*/
-	    loop: true,
-	    slidesPerView:3,
-	    slideToClickedSlide:true,
-	})
-	var mySwiper03 = new Swiper ('#swiper-container03', {
-	    loop: true,
-	    slidesPerView:1,
-	    loopAdditionalSlides:2   //复制后的slide数量要一样
-	})
-	mySwiper02.params.control = mySwiper03;  /*双向控制*/
-	mySwiper03.params.control = mySwiper02;
+	//首页news tab切换
+	tab("#swiper-container02",3,"#swiper-container03");
+	
+	//center tab切换
+	var numPerView = $("#swiper-container04").find(".tab").length;
+		numPerView = numPerView <= 4 ? numPerView :4;
+	tab("#swiper-container04",numPerView,"#swiper-container05");
 	
 	/*sideBar*/
 	$(".sideBarBtn").on("tap click",function(){
               $(".affix_p_sideBar .siderBar01").toggleClass("on");
+              background(this);
     })
 
 	//bottomBar 
@@ -45,6 +37,52 @@ $(function(){
 	        }
 	    })
     })
-    	
     
 })
+
+function background(trigger){
+	var b = $(".bg");
+	if(b.length == 0){
+		b = $("<div class='bg'/>");
+		b.appendTo("body");	
+		b.on("tap click",function(){$(trigger).trigger("tap")});
+	}
+	b.toggleClass("on");
+}
+
+function tab(tab,perView,slider){
+		if($(tab).length == 0)return;
+		var num01 = $(slider).find(".swiper-slide").length;
+		var status = num01 > perView ? true : false;
+		var mySwiper02;	
+    	if(status){
+    		mySwiper02 = new Swiper (tab, {
+			    /*autoplay:5000,*/
+			    loop: status,
+			    slidesPerView:perView,
+			    slideToClickedSlide:true
+			})
+    	}else{
+    		$(tab).find(".swiper-slide").eq(0).addClass("swiper-slide-active")
+    		$(tab).find(".swiper-slide").css("width",100/perView+"%").on("click",function(){
+    			mySwiper03.slideTo( $(this).index())
+    		});
+    	}
+
+		var mySwiper03 = new Swiper (slider, {
+		    loop: status,
+		    onSlideChangeStart: function(swiper){
+		      if(!status){
+		      	    $(tab).find(".tab ").removeClass('swiper-slide-active')
+		            $(tab).find(".tab ").eq(swiper.activeIndex).addClass('swiper-slide-active') 	 	
+		      }
+		    },
+		    slidesPerView:1,
+		    autoHeight: true,
+		    loopAdditionalSlides:(status?mySwiper02.slides.length:num01-num01)/2-1   //复制后的slide数量要一样
+		})
+		if(status){
+			mySwiper02.params.control = mySwiper03;  /*双向控制*/
+			mySwiper03.params.control = mySwiper02;
+	    }
+}
